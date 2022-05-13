3fb1a99fa189c6e5af72d15a1fb7362d2eca5e46
Vinícius Lucas <viniciuslucas1564@gmail.com>
2022-05-13 12:06:01 -0300

    Added text field component

---
+++ WpfMvvmExample.Presentation/Components/TextFieldComponent.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace WpfMvvmExample.Presentation.Components;
+
+public partial class TextFieldComponent : UserControl
+{
+    public static readonly DependencyProperty LabelProperty =
+        DependencyProperty.Register("Label", typeof(string), typeof(TextFieldComponent));
+    public static readonly DependencyProperty TextProperty =
+        DependencyProperty.Register("Text", typeof(string), typeof(TextFieldComponent));
+
+    public string Label
+    {
+        get => (string)GetValue(LabelProperty);
+        set => SetValue(LabelProperty, value);
+    }
+
+    public string Text
+    {
+        get => (string)GetValue(TextProperty);
+        set
+        {
+            SetValue(TextProperty, value);
+        }
+    }
+
+    public TextFieldComponent()
+    {
+        InitializeComponent();
+    }
+}
